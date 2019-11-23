e789f428e1ceacc6031fefa4a6ca5ba4977e88a1
hg8i <aaronsw@umich.edu>
2019-11-24 00:13:07 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 
 # setup script for linkii
 
-unlink ~/.vimbrc; ln -s .vimbrc ../.vimbrc
-unlink ~/.vimrc; ln -s .vimrc ../.vimrc
-unlink ~/.xinitrc; ln -s .xinitrc ../.xinitrc
-unlink ~/.bashrc; ln -s .bashrc ../.bashrc
+unlink ~/.vimbrc; ln -s dotFiles/.vimbrc ../.vimbrc
+unlink ~/.vimrc; ln -s dotFiles/.vimrc ../.vimrc
+unlink ~/.xinitrc; ln -s dotFiles/.xinitrc ../.xinitrc
+unlink ~/.bashrc; ln -s dotFiles/.bashrc ../.bashrc
 
