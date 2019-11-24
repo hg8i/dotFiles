e589342e04dd22be84f2b1a5d912dd623c751bd7
hg8i <aaronsw@umich.edu>
2019-11-24 20:04:04 +0100

    xbindkeys

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ unlink ~/.vimbrc; ln -s dotFiles/.vimbrc ../.vimbrc
 unlink ~/.vimrc; ln -s dotFiles/.vimrc ../.vimrc
 unlink ~/.xinitrc; ln -s dotFiles/.xinitrc ../.xinitrc
 unlink ~/.bashrc; ln -s dotFiles/.bashrc ../.bashrc
-unlink ~/.Xresources; ln -s dotFiles/.bashrc ../.Xresources
+unlink ~/.Xresources; ln -s dotFiles/.Xresources ../.Xresources
+unlink ~/.xbindkeysrc; ln -s dotFiles/.xbindkeysrc ../.xbindkeysrc
 
