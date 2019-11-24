ab2288de5ed4ed81de95a939720d5b34acac8737
hg8i <aaronsw@umich.edu>
2019-11-24 18:48:43 +0100

    X resources

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ unlink ~/.vimbrc; ln -s dotFiles/.vimbrc ../.vimbrc
 unlink ~/.vimrc; ln -s dotFiles/.vimrc ../.vimrc
 unlink ~/.xinitrc; ln -s dotFiles/.xinitrc ../.xinitrc
 unlink ~/.bashrc; ln -s dotFiles/.bashrc ../.bashrc
+unlink ~/.Xresources; ln -s dotFiles/.bashrc ../.Xresources
 
